fd71c9ff420535771a5fe752e9e6d0a8ef4166b8
Nicolina Artz <nicky@Nicolinas-MacBook-Pro-8.local>
2025-10-27 14:44:38 -0400

    Add welcome message and title

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -1,15 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget
 
 app = QApplication(sys.argv)
+#app = QApplication([])
 
 # create the window
 window = QWidget()
-window.setWindowTitle("My First PyQt App")
-window.setGeometry(100, 100, 400, 300) # x, y, width, height
+window.setWindowTitle("TrailRemover")
+window.setGeometry(100, 100, 1300, 750) # x, y, width, height
+introMsg = QLabel("<h1>Welcome to the Trail Remover application!</h1>", parent=window)
+introMsg.move(60, 15)
 
 # show the window
 window.show()
 
-# start the application's event loop
+# run the application's main loop
 sys.exit(app.exec_())
