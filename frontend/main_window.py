7e9a5c15557c33781d0ac2841839b645668ce62e
Nicolina Artz <nicky@Nicolinas-MacBook-Pro-8.local>
2025-10-31 13:25:41 -0400

    Combine and add file dialog

--- frontend/main_window.py
+++ frontend/main_window.py
@@ -1,5 +1,7 @@
+import os
 import sys
 
+from pathlib import Path
 from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.uic import loadUi
@@ -17,7 +19,7 @@ from PyQt5.QtWidgets import (
     QFileDialog
 )
 
-class LoadImageWindow(QWidget):
+class LoadImageWindow(QDialog):
     """
     This window is displayed once the user clicks the'Load Image'
     button on the main window. It allows the user to add FITS images
@@ -25,19 +27,29 @@ class LoadImageWindow(QWidget):
     """
     def __init__(self):
         super(LoadImageWindow, self).__init__()
-        loadUi("load_image.ui", self)
+
+        self.setWindowTitle("Load Image")
+        self.setFixedWidth(400)
+        self.setFixedHeight(300)
+
+        ui_path = Path(__file__).parent / "load_image.ui"
+        loadUi(str(ui_path), self)
+        self.browse.clicked.connect(self.browse_files)
+
+    def browse_files(self):
+        fname = QFileDialog.getOpenFileName(self, 'Load Image', os.path.expanduser('~'), 'FITS Images (*.fit)') # self, name, path, filetype
+        self.filename.setText(fname[0])
 
         #layout = QVBoxLayout()
         #self.label = QLabel("Load Image window")
         #layout.addWidget(self.label)
         #self.setLayout(layout)
 
-class Window(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         # Set the title and size of the project
         self.setWindowTitle("Trail Remover")
-        #self.setFixedSize(QSize(1300, 750))
 
         # Give the user a nice welcome message as soon as they open the GUI
         welcomeMsg = QLabel("<h1>Welcome to the Trail Remover application!</h1>")
@@ -57,61 +69,47 @@ class Window(QMainWindow):
         toolbar = QToolBar("This is the one and only toolbar")
 
         # add "Exit" button, which will exit the program when clicked
-        toolbar.addAction("Exit", self.close)
+        exit_button = toolbar.addAction("Exit", self.close)
+        exit_button.setStatusTip("Clicking this button will exit the program. Are you sure?")
 
         # add "Load Image" button, which will take the user to a new window
-        load_image = QAction("Load Image", self)
+        load_image = QPushButton("Load Image")
+        load_image.clicked.connect(self.show_new_window)
 
-        # let the user know what this button does
+        # let the user know what this button does & add it to the toolbar
         load_image.setStatusTip("Click here to pull up the Load Image screen!")
-        
-        # activate the button
-        load_image.triggered.connect(self.toolbar_button_clicked)
-        load_image.setCheckable(True)
-        toolbar.addAction(load_image)
+        toolbar.addWidget(load_image)
 
-        # open a new window once the button is clicked
-        load_image_2 = QPushButton("Load_Image_2.0")
-        load_image_2.clicked.connect(self.show_new_window)
-        #toolbar.addAction(load_image_2)
-        toolbar.addWidget(load_image_2)
-
-        # add the toolbar!
+        # add the toolbar itself!
         self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, toolbar)
-    
-    def toolbar_button_clicked(self, s):
-        # update the program when a toolbar button is clicked
-        print("click", s)
 
-    def show_new_window(self, checked):
-        if self.window is None:
-            self.window = LoadImageWindow()
-            self.window.show()
-        else:
-            #self.widow.close()
-            self.window = None
+    def show_new_window(self):
+        dialog = LoadImageWindow()
+        dialog.exec_()
 
     def _createStatusBar(self):
         status = QStatusBar()
         
         # add a default message to the status bar
-        status.showMessage("Not loading anything right now...")
+        status.showMessage("")
 
         # add the status bar!
         self.setStatusBar(status)
 
 if __name__ == "__main__":
-    #app = QApplication([])
-    #window = Window()
-    #window.setGeometry(100, 100, 1300, 750)
-    #window.show()
+    app = QApplication([])
+    window = MainWindow()
+    window.setGeometry(100, 100, 1300, 750)
+    window.show()
 
+    """
     app = QApplication(sys.argv)
     load_image_window = LoadImageWindow()
-    widget = QtWidgets.QtStackedWidget()
+    widget = QtWidgets.QStackedWidget()
     widget.addWidget(load_image_window)
     widget.setFixedWidth(400)
     widget.setFixedHeight(300)
     widget.show()
+    """
 
     sys.exit(app.exec())
